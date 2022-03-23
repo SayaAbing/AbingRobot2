d380ac7cedab4d4046864b6a03fe777f8ccb2410
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 15:12:06 +0700

    Update locks_sql.py

--- AbingRobot/modules/sql/locks_sql.py
+++ AbingRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SiestaRobot.modules.sql import SESSION, BASE
+from AbingRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
