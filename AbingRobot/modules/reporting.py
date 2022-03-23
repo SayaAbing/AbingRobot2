b0bc27555caf0d49cba55a086cebb651470c2def
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:20:04 +0700

    Update reporting.py

--- AbingRobot/modules/reporting.py
+++ AbingRobot/modules/reporting.py
@@ -1,9 +1,9 @@
 import html
 
-from SiestaRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from SiestaRobot.modules.log_channel import loggable
-from SiestaRobot.modules.sql import reporting_sql as sql
+from AbingRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from AbingRobot.modules.log_channel import loggable
+from AbingRobot.modules.sql import reporting_sql as sql
 from telegram import Chat, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
@@ -14,7 +14,7 @@ from telegram.ext import (
     MessageHandler,
 )
 from telegram.utils.helpers import mention_html
-from SiestaRobot.modules.language import gs
+from AbingRobot.modules.language import gs
 
 REPORT_GROUP = 12
 REPORT_IMMUNE_USERS = DRAGONS + TIGERS + WOLVES
