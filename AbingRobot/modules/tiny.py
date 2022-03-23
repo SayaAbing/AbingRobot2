b5ec85b030147d47d4005686ddbef5bf46afbb00
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:32:09 +0700

    Update tiny.py

--- AbingRobot/modules/tiny.py
+++ AbingRobot/modules/tiny.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 from PIL import Image
-from SiestaRobot.events import register
-from SiestaRobot import telethn as tbot
+from AbingRobot.events import register
+from AbingRobot import telethn as tbot
 
 
 @register(pattern="^/tiny ?(.*)")
@@ -13,7 +13,7 @@ async def _(event):
            return
     kontol = await event.reply("`Processing tiny...`")
     ik = await tbot.download_media(reply)
-    im1 = Image.open("SiestaRobot/resources/siesta_stickers.png")
+    im1 = Image.open("AbingRobot/resources/abing_stickers.png")
     if ik.endswith(".tgs"):
         await tbot.download_media(reply, "ken.tgs")
         os.system("lottie_convert.py ken.tgs json.json")
