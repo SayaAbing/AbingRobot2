df29faef281b0c97d9bd7a53873f9401555473b1
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:23:49 +0700

    Update shell.py

--- AbingRobot/modules/shell.py
+++ AbingRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from SiestaRobot import LOGGER, dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import dev_plus
+from AbingRobot import LOGGER, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
