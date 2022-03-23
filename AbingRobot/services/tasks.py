285fc6c3c1038403d2aa4426d655f6d2525491af
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 14:33:33 +0700

    Update tasks.py

--- AbingRobot/services/tasks.py
+++ AbingRobot/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from SiestaRobot import BOT_ID, BOT_USERNAME, DEV_USERS
-from SiestaRobot.services.sections import bold, section, w
+from AbingRobot import BOT_ID, BOT_USERNAME, DEV_USERS
+from AbingRobot.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
