54c7068595653bf755a49f13a4cc07570c13f99e
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:43:14 +0700

    Update weather.py

--- AbingRobot/modules/weather.py
+++ AbingRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SiestaRobot import *
+from AbingRobot import *
 
-from SiestaRobot import telethn as tbot
-from SiestaRobot.events import register
+from AbingRobot import telethn as tbot
+from AbingRobot.events import register
 
 
 async def is_register_admin(chat, user):
