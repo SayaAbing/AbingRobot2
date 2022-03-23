ff792ad68001f150f2ef239bc7bd3c58ce06431f
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 14:53:05 +0700

    Update antiflood_sql.py

--- AbingRobot/modules/sql/antiflood_sql.py
+++ AbingRobot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, BigInteger, UnicodeText
 
-from SiestaRobot.modules.sql import SESSION, BASE
+from AbingRobot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
