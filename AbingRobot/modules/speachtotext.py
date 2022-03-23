08a587d4c56bcada9b0a4b71bf201ccb744d152b
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:25:21 +0700

    Update speachtotext.py

--- AbingRobot/modules/speachtotext.py
+++ AbingRobot/modules/speachtotext.py
@@ -10,9 +10,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SiestaRobot import *
-from SiestaRobot.events import register
-from SiestaRobot import telethn as tbot
+from AbingRobot import *
+from AbingRobot.events import register
+from AbingRobot import telethn as tbot
 
 
 async def is_register_admin(chat, user):
