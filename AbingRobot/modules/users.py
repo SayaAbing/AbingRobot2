1afa6f1f0fef166efb41fa6154d1a0cb434b482c
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:40:53 +0700

    Update users.py

--- AbingRobot/modules/users.py
+++ AbingRobot/modules/users.py
@@ -10,10 +10,10 @@ from telegram.ext import (
     MessageHandler,
 )
 
-import SiestaRobot.modules.sql.users_sql as sql
-from SiestaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from SiestaRobot.modules.sql.users_sql import get_all_users
+import AbingRobot.modules.sql.users_sql as sql
+from AbingRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from AbingRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
