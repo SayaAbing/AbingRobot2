ee6a2ec6fea7abbd3e52ff48785ae8df091443d6
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 14:22:45 +0700

    Update config.py

--- AbingRobot/config.py
+++ AbingRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SiestaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/AbingRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,17 +17,17 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 945137470  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "saint_foire"
-    SUPPORT_CHAT = "machinaxsupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1337194042  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "sayaabing"
+    SUPPORT_CHAT = "AbingSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001180007354
+        -1001782974167
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001237968047
+        -1001782974167
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     ERROR_LOGS = (
-        -1001732618654
+        -1001782974167
     )  # Prints information Error
 
     # RECOMMENDED
