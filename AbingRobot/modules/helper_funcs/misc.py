982ea6e5cc762e8fa595d59771cfb00fcb22538b
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 15:20:14 +0700

    Bapa nya 1

--- AbingRobot/modules/helper_funcs/misc.py
+++ AbingRobot/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from SiestaRobot import NO_LOAD
+from AbingRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="siesta_back")]]
+        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="abing_back")]]
 
     return pairs
 
