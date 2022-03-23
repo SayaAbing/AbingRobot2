79d34e0074466631448b6c21d009fec6e30da24f
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:26:54 +0700

    Update stickers.py

--- AbingRobot/modules/stickers.py
+++ AbingRobot/modules/stickers.py
@@ -13,9 +13,9 @@ from telegram import TelegramError, Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.disable import DisableAbleCommandHandler
-from SiestaRobot.modules.language import gs
+from AbingRobot import dispatcher
+from AbingRobot.modules.disable import DisableAbleCommandHandler
+from AbingRobot.modules.language import gs
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
