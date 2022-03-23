db9871cfdff211c0b55a049dcead9807eff5d205
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-24 00:05:05 +0700

    Update myanimelist.py

--- AbingRobot/modules/myanimelist.py
+++ AbingRobot/modules/myanimelist.py
@@ -14,7 +14,7 @@ from telegram import (
 )
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 
-from SiestaRobot import dispatcher
+from AbingRobot import dispatcher
 
 jikan = Jikan()
 
