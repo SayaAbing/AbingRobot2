5a59edde313051d481b2a57f1c9e5b7920460157
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 20:44:40 +0700

    Jangan Ego Jangan

--- AbingRobot/modules/eval.py
+++ AbingRobot/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from SiestaRobot import LOGGER, dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import dev_plus
+from AbingRobot import LOGGER, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
