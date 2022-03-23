9f895fbbd9d3bbd106038b3ffa5afcbba4e852a2
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-23 21:02:16 +0700

    Update imdb.py

--- AbingRobot/modules/imdb.py
+++ AbingRobot/modules/imdb.py
@@ -1,11 +1,11 @@
-from SiestaRobot import telethn as tbot
+from AbingRobot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from SiestaRobot.events import register
+from AbingRobot.events import register
 
 langi = "en"
 
